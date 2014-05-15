9171bce3176edc483c4a7be58547addd7b767b78
Brendan Brown <brendanbrown27@gmail.com>
2014-05-15 14:47:41 -0700

    full makeover

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,102 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function ready(){
+	$('#one').text("");
+    $('#two').text("R");
+    $('#three').text("eady")
+    $('#play').prop("disabled", false);
+    $('#search-input').val("");
+}
+
+function menu_maker_wiki(data){
+   $('#menu').remove();
+   $('#menu-box').append("<ul id='menu'>");
+   console.log(data.outline);
+   console.log(data.broker);
+   	for (var i = 0; i < data.outline.length; i++) {
+      	if(i == 0){
+        	$('#menu').append("<li><button class='menu-button' art="+i+">" + data.outline[i].replace("[edit]", "") + "</button></li><br>");
+      	}
+      	else{
+        	if(data.broker[i+1] != null){
+            	$('#menu').append("<li><button class='menu-button' art=" + (i + 1) + ">" + data.outline[i].replace("[edit]", "") + "</button></li><br>");
+         	}
+      	}
+   	};
+   	
+   	$('#menu-box').append("</ul");
+   	$('#menu-box').hide()
+};
+
+
+
+function menu_maker_reddit(data_titles, data_sources, data_links){
+   $('#menu').remove();
+   $('#menu-box').append("<ul id='menu'>");
+   
+   for (var i = 0; i < 20; i++) {
+      	$('#menu').append("<li art-src='" + data_links[i] + "'><button class='menu-button-reddit'><b>" + (i+1) + ") " + data_titles[i] + "</b> " + data_sources[i] + "</button></li><br>");
+   	};
+   	
+   	$('#menu-box').append("</ul>");
+   	$('#menu-box').hide()
+}
+
+
+function changeup(words, counter){
+   	var word = words[counter];
+   	var part1 = '';
+   	var part2 = '';
+   	var redlet = '';
+   	console.log(words.length, counter);
+
+   	if(word.length == 0){
+      	part1 = ' ';
+ 		part2 = ' ';
+ 		redlet = ' ';
+ 	}
+   
+   	else if(word.length == 1){
+      	redlet = word;
+   	}
+   
+   	else if(word.length == 2){;
+      	part1 = word[0];
+      	redlet = word[1];
+   	}
+   
+   	else if(word[1] == 'h'){
+      	part1 = word[0];
+      	redlet = word[1];
+      	for (var i = 2; i < word.length; i++) {
+        	part2 = part2 + word[i];
+      	}; 
+   	}
+   
+   	else{
+     
+      	for (var i = 0; i < word.length; i++) {
+        	if(i < word.length / 4){
+            	part1 += word[i];
+        	}
+        	else{
+            	part2 += word[i];
+        	}
+    	};
+     
+	    redlet = part2[0];
+	    part2 = part2.slice(1, part2.length);
+  
+   	}
+
+   	$('#one').text(part1);
+   	$('#two').text(redlet);
+   	$('#three').text(part2);
+  	
+  	if(counter === words.length){
+    	console.log('DONE!');
+  	}
+
+}//END OF CHANGEUP   
--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -15,10 +15,12 @@ class ReaderController < ApplicationController
   		
   		link = ""
   		wikipedia = false
+  		reddit = false
   		
-  		if(params[:link].include? "http:")
+  		if(params[:link].include? "reddit")
   			link = params[:link]
-  		elsif(params[:link].include? "https:")
+  			reddit = true
+  		elsif(params[:link].include? "http")
   			link = params[:link]
   		else
   			link = "https://en.wikipedia.org/wiki/" + params[:link].split.map(&:capitalize).join(' ').gsub(" ", "_")
@@ -30,31 +32,68 @@ class ReaderController < ApplicationController
 		
 		page = Nokogiri::HTML(open(link))
 		content = page.css('p').collect{ |node| node.text }.join(" ").gsub(/\[([^\]]+)\]/, "")
-		outline = page.css('h2').collect{ |node| node.text }
-		array = page.css('div#mw-content-text').search('h2, p').map &:text
-
+		
+		outline = ""
 		broker = []
-		count = 0
+		links = []
+		titles = []
+		sources = []
 
-		array.each do |entry|
+		if wikipedia == true	
+			outline = page.css('h2').collect{ |node| node.text }
+			array = page.css('div#mw-content-text').search('h2, p').map &:text
+		
+			count = 0
 
-			if !outline.include? entry
-				if !broker[count]
-					broker[count] = ""
-					broker[count] += entry.to_s.gsub(/\[([^\]]+)\]/, "")
+			array.each do |entry|
+				if !outline.include? entry
+					if !broker[count]
+						broker[count] = ""
+						broker[count] += entry.to_s.gsub(/\[([^\]]+)\]/, "")
+					else
+						broker[count] += " " + entry.to_s.gsub(/\[([^\]]+)\]/, "")
+					end
 				else
-					broker[count] += " " + entry.to_s.gsub(/\[([^\]]+)\]/, "")
+					count += 1
 				end
-			else
-				count += 1
 			end
-		end
-		
-		#broker and outline are same length
-		#broker 1 is blank
 
+		end
+
+		if reddit == true
+
+			titles = page.css('a.title').map &:text
+			sources = page.css('span.domain').map &:text
+			links = page.css('a.title').map {|link| link['href'] }
+			
+			# puts titles
+			# puts sources
+			# puts links
+
+		end
+
+		type = ""
+		if wikipedia == true
+			type = "wikipedia"
+		elsif reddit == true
+			type = "reddit"
+		elsif params[:menukeeper] == "yes"
+			type = "news"
+		else
+			type = "normal"
+		end
+			
 		
-		json_message = {:status => 'success', :message => content, :outline => outline, :broker => broker}
+		json_message = {:status => 'success', 
+			:type => type,
+			:message => content, 
+			:outline => outline, 
+			:broker => broker, 
+			:titles => titles,
+			:sources => sources,
+			:links => links
+		}
+  		
   		render json: json_message
 
 	end
